191e359005cf85bb69f0b7e10cfb4477d8a4333f
Shailove <124027365+shailovesingh@users.noreply.github.com>
2025-09-27 00:42:31 +0530

    git commit -m "Chat with web"

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -33,4 +33,10 @@ class LoadStreamlitUI:
             ## USecase selection
             self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)
 
+            if self.user_controls["selected_usecase"] == "Chatbot with Web":
+                os.environ["TAVILY_API_KEY"] = self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"]=st.text_input("TAVILY_API_KEY",type="password")
+
+                if not self.user_controls["TAVILY_API_KEY"]:
+                    st.warning("Please enter your Tabily Api key to proceed")
+
         return self.user_controls
